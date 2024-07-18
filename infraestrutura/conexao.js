b71629d67af9ae75e0e787e52feb70b458af32b3
Leonardo Carneiro <69646391+Leonardo-Carneir0@users.noreply.github.com>
2024-07-17 23:31:52 -0300

    (fix) ajustes finais

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -9,4 +9,12 @@ const connection = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 
+console.log({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
 module.exports = connection;
